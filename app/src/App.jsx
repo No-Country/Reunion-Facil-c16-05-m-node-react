6af0579e7607cda0b81070902622424761ab254a
Franco Dionofrio <dionofrio_franco@hotmail.com>
2024-02-24 15:26:32 -0300

    Add Element Form

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,12 @@
+import { EventCreationForm } from './pages/EventCreationForm'
 import './App.css'
 
 function App () {
   return (
-   <h1>Inicio de la App Reunion Facil</h1>
+    <>
+      {/* <h1>Inicio de la App Reunion Facil</h1> */}
+      <EventCreationForm />
+    </>
   )
 }
 

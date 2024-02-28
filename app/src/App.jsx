012123a8c57e39f87651b9ecc3fd3a0146493b05
Franco Dionofrio <dionofrio_franco@hotmail.com>
2024-02-28 18:35:30 -0300

    Add Weekday

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,15 @@
-import { EventCreationForm } from './pages/EventCreationForm'
 import './App.css'
+import { Calendar } from './components/Calendar/Calendar'
+import { CalendarProvider } from './context/CalendarContext'
 
 function App () {
   return (
     <>
       {/* <h1>Inicio de la App Reunion Facil</h1> */}
-      <EventCreationForm />
+      <CalendarProvider>
+        {/* //TODO: Input Calendar */}
+        <Calendar />
+      </CalendarProvider>
     </>
   )
 }

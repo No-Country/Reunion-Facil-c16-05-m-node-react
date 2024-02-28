6d0f4a69d637fc45670a45ff25f4cda6a7c20172
Catalina Lima Brun <catalinalbrun@gmail.com>
2024-02-28 12:10:33 -0700

    Context Updated

--- app/src/components/ClockCard/ClockCard.jsx
+++ app/src/components/ClockCard/ClockCard.jsx
@@ -1,37 +1,17 @@
-import { useState } from 'react'
 import style from './ClockCard.module.css'
+import { useClockContext } from '../../context/ClockContext.jsx'
 
 function ClockCard () {
-  const [selectedHour, setSelectedHour] = useState('00')
-  const [selectedMinutes, setSelectedMinutes] = useState('00')
-  const [selectedPeriod, setSelectedPeriod] = useState('')
-  const [accepted, setAccepted] = useState(false)
-
-  const handleHourChange = (e) => {
-    const hour = e.target.value
-    if (hour === '' || (/^\d+$/.test(hour) && Number(hour, 10) >= 0 && Number(hour, 10) <= 24)) {
-      setSelectedHour(hour)
-    }
-  }
-
-  const handleMinutesChange = (e) => {
-    const minutes = e.target.value
-    if (minutes === '' || (/^\d+$/.test(minutes) && parseInt(minutes, 10) >= 0 && parseInt(minutes, 10) <= 59)) {
-      setSelectedMinutes(minutes)
-    }
-  }
-
-  const handlePeriodChange = (period) => {
-    setSelectedPeriod(period)
-  }
-
-  const handleCancel = () => {
-    // To do: CANCEL and close pop up
-  }
-
-  const handleAccept = () => {
-    setAccepted(true)
-  }
+  const {
+    selectedHour,
+    selectedMinutes,
+    selectedPeriod,
+    handleHourChange,
+    handleMinutesChange,
+    handlePeriodChange,
+    handleCancel,
+    handleAccept
+  } = useClockContext()
 
   return (
     <div className={style['clock-card']}>
@@ -44,23 +24,23 @@ function ClockCard () {
                 className={style.hourSelector}
                 type="text"
                 value={selectedHour}
-                onChange={handleHourChange}
+                onChange={(e) => handleHourChange(e.target.value)}
                 maxLength={2}
               />
               <label className={style.label}>Hora</label>
-              </div>
-              <span className={style.span}>:</span>
-              <div className={style.input}>
+            </div>
+            <span className={style.span}>:</span>
+            <div className={style.input}>
               <input
                 className={style.hourSelector}
                 type="text"
                 value={selectedMinutes}
-                onChange={handleMinutesChange}
+                onChange={(e) => handleMinutesChange(e.target.value)}
                 maxLength={2}
               />
               <label className={style.label}>Minutos</label>
-              </div>
-              <div className={style['period-buttons']}>
+            </div>
+            <div className={style['period-buttons']}>
               <button
                 className={`${style['period-button']} ${
                   selectedPeriod === 'AM' ? style.selected : ''
@@ -77,25 +57,25 @@ function ClockCard () {
               >
                 PM
               </button>
-              </div>
             </div>
-            <div className={style['action-buttons']}>
+          </div>
+          <div className={style['action-buttons']}>
             <button
-              className={`${style['cancel-button']} ${accepted ? style.accepted : ''}`}
+              className={`${style['cancel-button']} ${style.accepted}`}
               onClick={handleCancel}
             >
               Cancelar
             </button>
             <button
-              className={`${style['accept-button']} ${accepted ? style.accepted : ''}`}
+              className={`${style['accept-button']} ${style.accepted}`}
               onClick={handleAccept}
             >
               Aceptar
             </button>
           </div>
-          </div>
         </div>
       </div>
+    </div>
   )
 }
 
--- app/src/context/clockContext.jsx
+++ app/src/context/clockContext.jsx
@@ -1,24 +1,50 @@
 import { createContext, useState } from 'react'
 
-export const clockContext = createContext()
+export const ClockContext = createContext()
 
 export function ClockProvider ({ children }) {
   const [selectedHour, setSelectedHour] = useState('00')
   const [selectedMinutes, setSelectedMinutes] = useState('00')
   const [selectedPeriod, setSelectedPeriod] = useState('')
 
+  const handleHourChange = (hour) => {
+    if (hour === '' || (/^\d+$/.test(hour) && Number(hour, 10) >= 0 && Number(hour, 10) <= 24)) {
+      setSelectedHour(hour)
+    }
+  }
+
+  const handleMinutesChange = (minutes) => {
+    if (minutes === '' || (/^\d+$/.test(minutes) && parseInt(minutes, 10) >= 0 && parseInt(minutes, 10) <= 59)) {
+      setSelectedMinutes(minutes)
+    }
+  }
+
+  const handlePeriodChange = (period) => {
+    setSelectedPeriod(period)
+  }
+
+  const handleCancel = () => {
+    // TODO: Cancel and close pop up
+  }
+
+  const handleAccept = () => {
+    // TO DO: Accept and send data to backend
+  }
+
   const contextValues = {
     selectedHour,
     selectedMinutes,
     selectedPeriod,
-    setSelectedHour,
-    setSelectedMinutes,
-    setSelectedPeriod
+    handleHourChange,
+    handleMinutesChange,
+    handlePeriodChange,
+    handleCancel,
+    handleAccept
   }
 
   return (
-  <clockContext.Provider value={contextValues}>
-    {children}
-  </clockContext.Provider>
+    <ClockContext.Provider value={contextValues}>
+      {children}
+    </ClockContext.Provider>
   )
 }

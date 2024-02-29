ab4b5b3b61131d324ec333d511c62a29c12d9bc3
Franco Dionofrio <dionofrio_franco@hotmail.com>
2024-02-29 18:11:36 -0300

    Update icon position

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
-import { Calendar } from './components/Calendar/Calendar'
-import { CalendarProvider } from './context/CalendarContext'
+import { EventCreationForm } from './pages/EventCreationForm'
 
 function App () {
   return (
     <>
       {/* <h1>Inicio de la App Reunion Facil</h1> */}
-      <CalendarProvider>
-        {/* //TODO: Input Calendar */}
-        <Calendar />
-      </CalendarProvider>
+      <EventCreationForm />
     </>
   )
 }

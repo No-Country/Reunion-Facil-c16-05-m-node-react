508162071cfcd88af3aa743e82aa3ee02435c71d
jeisonTrabajo <jw.vera.r@gmail.com>
2024-03-01 15:08:39 -0500

    Co-authored-by: Santiago Flores <AgustinCrow@users.noreply.github.com>

--- backend/controllers/meeting.controller.js
+++ backend/controllers/meeting.controller.js
@@ -1,13 +1,15 @@
-import Meeting from '../models/meetingModels.js';
-import Suggestion from '../models/sugesttionModels.js';
-import User from '../models/userModels.js';
+import Meeting from "../models/meetingModels.js";
+import Suggestion from "../models/sugesttionModels.js";
+import User from "../models/userModels.js";
+import Guest from "../models/guestModels.js";
 
 // Create and Save a new Meeting
+//Datos necesarios: title, id_user_org, suggestion{datetime, location}, guest{guest}
 const createMeeting = async (req, res) => {
-  // Validate request
-  if (!req.body.title) {
+  // Validate request tittle don't undefined
+  if (!req.body.title || !req.body.id_user_org) {
     res.status(400).send({
-      message: "Content can not be empty!"
+      message: "Content can not be empty!",
     });
     return;
   }
@@ -16,33 +18,48 @@ const createMeeting = async (req, res) => {
   const meeting = {
     title: req.body.title,
     description: req.body.description,
-    id_user_org: req.body.id_user_org
+    id_user_org: req.body.id_user_org,
+  };
 
-  }
+  //create a suggestion
   const suggestion = {
     datetime: req.body.datetime,
     location: req.body.location,
-    description_location: req.body.description_location
+    description_location: req.body.description_location,
   };
-
-
+  const guests = req.body.guests;
 
   // Save Meeting in the database
+
+  const newSuggestion = await Suggestion.create(suggestion);
+  const data = await Meeting.create(meeting);
+  const relationship = await data.setSuggestion(newSuggestion);
   try {
-    const newSuggestion = await Suggestion.create(suggestion);
-    const data = await Meeting.create(meeting);
-    const relationship = await data.setSuggestion(newSuggestion)
-   return  res.status(201).json(relationship);
-  }
-  catch (error) {
-   return  res.status(500).json({
+    for (const guest of guests) {
+      await Guest.create({
+        user_guest: guest,
+        id_meeting: data.getDataValue("id_meeting"),
+      });
+    }
+
+    return res.status(201).json({
+      ...relationship,
+      suggestion: {
+        datetime: newSuggestion.getDataValue("datetime"),
+        location: newSuggestion.getDataValue("location"),
+        description_location: newSuggestion.getDataValue(
+          "description_location"
+        ),
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
       message:
-        error.message || "Some error occurred while creating the Meeting."
+        error.message || "Some error occurred while creating the Meeting.",
     });
   }
 };
-
-
+//--Obtener reuniones de un usuario particular--
 
 const getMeetingByUser = async (req, res) => {
   const id = req.params.id;
@@ -54,16 +71,17 @@ const getMeetingByUser = async (req, res) => {
       },
       include: [
         {
-          model: User, attributes: ['nickname', 'username']
+          model: User,
+          attributes: ["nickname", "username"],
         },
         {
-          model: Suggestion, attributes: ['location', 'description_location', 'datetime']
-
-        }
+          model: Suggestion,
+          attributes: ["location", "description_location", "datetime"],
+        },
       ],
     });
     if (meeting.length === 0) {
-      return res.status(404).json({ message: `ID:${id}not found` });
+      return res.status(404).json({ message: `ID :${id} not found` });
     }
     return res.status(200).json(meeting);
   } catch (error) {

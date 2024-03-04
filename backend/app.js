3ed46ee13f2cc655ed9de2c99115af489eec1ac3
jeisonTrabajo <jw.vera.r@gmail.com>
2024-03-04 11:47:03 -0500

    update in cors origin delete method

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,7 @@ const app = express();
 
 
 //middleware
-app.use(cors({
-  origin:'http://localhost:5173',
-  credentials: true,
-  
-}));
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extends:false}));

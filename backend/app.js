c3964a822bd2426fc16d5b92e312ea7ee1827dd4
jeisonTrabajo <jw.vera.r@gmail.com>
2024-03-06 15:18:15 -0500

    create url database connection success

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,12 @@ const app = express();
 
 
 //middleware
-app.use(cors({
-  origin:'http://localhost:5173',
-  credentials: true,
-  
-}));
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extends:false}));
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("<h1>server</h1>");
 });
 

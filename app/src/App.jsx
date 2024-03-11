6ebc4428fe97628eac4878b3cd2b9f580955dab5
Franco Dionofrio <dionofrio_franco@hotmail.com>
2024-03-11 17:48:04 -0300

    Add GuestSelected

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import GuestSelected from './components/GuestSelected/GuestSelected'
 import { EventCreationForm } from './pages/EventCreationForm'
 
 function App () {
   return (
-    <EventCreationForm />
+    <GuestSelected />
   )
 }
 

61c9db3b11e5dab8e640e49ec87d48f371992050
Franco Dionofrio <dionofrio_franco@hotmail.com>
2024-03-14 21:10:05 -0300

    Add HeaderHome and routing to Home

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { EventCreationForm } from './pages/EventCreationForm'
+import { EventCreationForm } from './pages/EventCreationForm/EventCreationForm'
 import { Header } from './layout/Header/Header'
+import { HeaderHome } from './layout/HeaderHome/HeaderHome'
+import { HomeEvent } from './pages/HomeEvent/HomeEvent'
 
 function App () {
   return (
     <Routes>
+      <Route path='/' element={<HeaderHome />}>
+        <Route index element={<HomeEvent /> } />
+      </Route>
       <Route path='/crear-reunion' element={<Header />}>
         <Route index element={<EventCreationForm />} />
         <Route path='invitado' element={<h1>Pantalla de agregar invitado</h1>} />
